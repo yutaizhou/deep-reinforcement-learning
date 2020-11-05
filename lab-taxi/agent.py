8fe3778175bec473a37eea94f606b5f263f27974
Yutai Zhou <yutai.zhou@ll.mit.edu>
2020-11-05 08:26:05 -0500

    implementing q learning was easy enough, but holy shit hyperparam opt for eps and alpha is annoying... thankfully taxi v3 is harder than v2 so I am stopping below the 9.1 recommendation originally made for v2. also shoutout to the awesome eps decay visualizer https://observablehq.com/@katnoria/visualising-epsilon-decay'

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -12,6 +12,16 @@ class Agent:
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.alpha = 0.1
+        self.eps = 1
+        self.eps_decay = 0.9998
+
+    def _update_eps(self, current_episode, total_episode): 
+        # eps = 1 / current_episode
+        # self.eps = eps if eps >= 0.1 else 0.1
+        # if self.eps > 0.1:
+        self.eps *= self.eps_decay 
+        
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -24,9 +34,11 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
+        greedy_action = np.argmax(self.Q[state])
+        random_action = np.random.choice(self.nA)
+        return greedy_action if np.random.uniform() > self.eps else random_action
 
-    def step(self, state, action, reward, next_state, done):
+    def step(self, state, action, reward, next_state, done, i_episode, num_episodes):
         """ Update the agent's knowledge, using the most recently sampled tuple.
 
         Params
@@ -37,4 +49,9 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        target = reward + np.max(self.Q[next_state])
+        current = self.Q[state][action]
+        self.Q[state][action] += self.alpha * (target - current)
+
+        self._update_eps(i_episode, num_episodes)
+        
